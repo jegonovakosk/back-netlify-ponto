3760e64ff91262624c4ea91ccced3e66dde3c511
jegonovakosk <62294366+jegonovakosk@users.noreply.github.com>
2024-06-20 10:19:39 -0300

    ajuste para deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Handler, Context, Callback } from 'aws-lambda';
+import serverless from 'serverless-http';
+
+let server: Handler;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: ['error'] });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Permite CORS para o Netlify
+
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverless(expressApp);
 }
+
 bootstrap();
+
+export const handler: Handler = (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  if (!server) {
+    bootstrap().then(() => server(event, context, callback));
+  } else {
+    server(event, context, callback);
+  }
+};
