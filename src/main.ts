d2828d1bda145a3f6ce383619ac91a986b37f90b
jegonovakosk <62294366+jegonovakosk@users.noreply.github.com>
2024-06-20 16:01:50 -0300

    zerando p projeto

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express';
-
-let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  server = serverlessExpress({ app: expressApp });
+  await app.listen(3000);
 }
-
 bootstrap();
-
-export const handler = async (event: any, context: any) => {
-  if (!server) {
-    await bootstrap();
-  }
-  return server(event, context);
-};
